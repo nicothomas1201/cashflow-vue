8d655487e2fe2e85c09c514fe65d7cc9234bc7bc
Nicolás Quinero <quinterorodrigueznicolas@gmail.com>
2023-07-05 11:12:44 -0500

    componente movement and props

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,9 +5,9 @@
     </template>
     <template #resume>
       <Resume
-        :label="'Junio 2023'"
+        :label="label"
         :total-amount="100000"
-        :amount="10000"
+        :amount="amount"
       >
         <template #graphic>
           Graphic
@@ -18,7 +18,9 @@
       </Resume>
     </template>
     <template #movements>
-      <Movements />
+      <Movements
+        :movements="movements" 
+      />
     </template>
   </Layout>
 </template>
@@ -27,7 +29,7 @@
   import Layout from './Layout.vue';
   import Header from './Header.vue';
   import Resume from './Resume/Index.vue';
-  import Movements from './Movements.vue';
+  import Movements from './Movements/Index.vue';
 
   export default {
     components: {
@@ -35,6 +37,44 @@
       Header,
       Resume,
       Movements,
+    },
+    data(){
+      return {
+        label: null,
+        amount: null,
+        movements: [
+          {
+            id: 1,
+            title: "Movimiento",
+            description: "Deposito de salario",
+            amount: "1000",
+          },
+          {
+            id: 2,
+            title: "Movimiento 1",
+            description: "Deposito de honorarios",
+            amount: "500",
+          },
+          {
+            id: 3,
+            title: "Movimiento 3",
+            description: "Comida",
+            amount: "-100",
+          },
+          {
+            id: 4,
+            title: "Movimiento 4",
+            description: "Colegiatura",
+            amount: "1000",
+          },
+          {
+            id: 5,
+            title: "Movimiento 5",
+            description: "Reparación equipo",
+            amount: "1000",
+          },
+        ]
+      }
     }
   }
 </script>
