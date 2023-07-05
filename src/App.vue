3533f3adf348e1fe93fa76a97fb2ef3fa40d774b
Nicolás Quinero <quinterorodrigueznicolas@gmail.com>
2023-07-04 19:30:57 -0500

    suspense and loading

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
+<template>
+  <!-- A ver pringado, suspense utiliza dos slots, el # es para darle un nombre
+  el defualt se muestra cuando se carga, y el fallback mientras esta cargando -->
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />  
+    </template>
+  </Suspense>
+</template>
+
+
 <script>
-  import Home from './components/Home.vue';
+  import SplashScreen from './components/SplashScreen.vue';
+  import { defineAsyncComponent } from 'vue';
 
   export default {
     components: {
-      Home,
+      SplashScreen,
+      Home: defineAsyncComponent(() => 
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"))
+          }, 2500)
+        })
+      )
     }
   }
-
 </script>
 
-<template>
-  <Home />  
-</template>
-
 <style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
